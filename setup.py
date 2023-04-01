1c3cf12344261e296e7b9a93657963f029f63e52
Pau Andrio <andriopau@gmail.com>
2023-04-02 01:34:19 +0200

    Readme badges

--- setup.py
+++ setup.py
@@ -23,8 +23,13 @@ setuptools.setup(
     classifiers=(
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX",
+        "Operating System :: Unix",
+
     ),
 )
